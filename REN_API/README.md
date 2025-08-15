20ff43e9677a0423950153048de69363f6f95dc5
Rui Martins <up202104636@up.pt>
2025-08-15 20:16:56 +0100

    Update to the REN_API script

---
+++ REN_API/README.md
@@ -0,0 +1,14 @@
+# INSTRUCTIONS
+
+# .env
+The user must create a file named ".env". This file contains all the necessary data (API_URL, ...) to get data from REN's database.
+
+Template:
+```bash
+# REN's API
+API_URL = "https://servicebus.ren.pt/datahubapi/electricity/ElectricityMarketPricesDaily"
+
+# Parameters
+CULTURE = "pt-PT"
+DATE = "2025-08-15"
+```
---
+++ REN_API/RenApiCall.py
@@ -0,0 +1,48 @@
+import requests
+from datetime import datetime
+from pprint import pprint
+import os
+from dotenv import load_dotenv
+
+# API configuration
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
+CULTURE = os.getenv("CULTURE")
+DATE = os.getenv("DATE")
+
+
+def get_electricity_price(country:str = "pt-PT", date:datetime = datetime.now().strftime("%Y-%m-%d")):
+    """
+    Fetch daily electricity market prices from the REN DataHub API. Return JSON.
+    """
+    params = {
+        "culture": country,
+        "date": date
+    }
+    
+    try:
+        response = requests.get(API_URL, params=params)
+        response.raise_for_status()  # Raise an error for bad status codes
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Failed to fetch data: {str(e)}"}
+
+def main():
+    # Fetch market prices
+    result: dict = get_electricity_price(country=CULTURE, date=DATE)
+    
+    if "error" in result:
+        print(result["error"])
+    else:
+        print(f"Electricity Market Prices for {CULTURE} on {DATE}:")
+        if isinstance(result, dict):
+            # print(f"Price: {list( map(lambda x: round(x / 1000, 5), result["series"][0]["data"]) )} €/kWh")^
+            print(f"Price: { result["series"][0]["data"] } €/MWh")
+        else:
+            print("Data format not recognized.")
+        
+        #pprint(result)
+
+if __name__ == "__main__":
+    main()
